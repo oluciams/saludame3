7f2c22da08ee43bbe852f29181d590e142af0b69
olga <oluciams84@gmail.com>
2021-06-27 17:44:58 -0500

    implementando el motor de plantillas con hbs y con bodyParser para formularios

--- app.js
+++ app.js
@@ -2,28 +2,31 @@
 
 const express = require('express');
 const app = express();
+const path = require('path')
+const hbs = require('express-handlebars')
 
 const bodyParser = require('body-parser')
-
 app.use(bodyParser())
 
-app.get('/form', (req, res) => {
-  //console.log(req.headers['user-agent'])
-  console.log(req.headers.connection)
-  res.send(`
-    <form action="/hello" method="post">
-        <label for="name"><input type="text" id="name" name="name">
-        <button type="submit">Enviar</button>
-    </form>`
-    );
+app.set('views', path.join(__dirname, 'views'))
+
+app.engine('.hbs', hbs({
+  layoutsDir:path.join(app.get('views'), 'layouts'),
+  partialsDir:path.join(app.get('views'), 'partials'),
+  extname:'.hbs',
+  defaultLayout:'main'  
+}))
+
+app.set('view engine', 'hbs')
+
+app.get('/', (req, res) => {
+     res.render('index')
 });
 
-app.post('/hello', (req, res, next) => {
+app.post('/hello', (req, res) => {
   
-    res.status(200).send(`<h1>Hola ${req.body.name}!</h1>`) 
-    
-  });
-
+  res.render('saludo', {name: req.body.name} )     
+});
   
 
 app.listen(3000, () => console.log('Listening on port 3000!'));
