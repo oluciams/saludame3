f103fe0e2e972af7b6650e31d97e16d9e3ed0ee1
olga <oluciams84@gmail.com>
2021-06-27 16:43:56 -0500

    Corregi la carpeta para git ignore

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ const bodyParser = require('body-parser')
 app.use(bodyParser())
 
 app.get('/form', (req, res) => {
+  //console.log(req.headers['user-agent'])
+  console.log(req.headers.connection)
   res.send(`
     <form action="/hello" method="post">
         <label for="name"><input type="text" id="name" name="name">
@@ -16,9 +18,13 @@ app.get('/form', (req, res) => {
     );
 });
 
-app.post('/hello', (req, res) => {
-   res.send(`<h1>Hola ${req.body.name}!</h1>`)    
+app.post('/hello', (req, res, next) => {
+  
+    res.status(200).send(`<h1>Hola ${req.body.name}!</h1>`) 
+    
   });
+
+  
 
 app.listen(3000, () => console.log('Listening on port 3000!'));
 
